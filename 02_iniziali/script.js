d46470b97d6d7fc51bbe68364576510bf62ebcde
mattiacoli <mattia.coli@aol.com>
2025-02-17 15:53:23 +0100

    add iniziali function

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -3,10 +3,46 @@
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
+
+/* 
+  let iniziali = []
+  for (let i = 0; i < names.length; i++) {
+  const nome = names[i];
+  inziali.push(nome[0])  
+}
+console.log(inziali);
+ */
+
 // Dichiara la funzione qui.
+
+/* function arrDiIniziali(arr) {
+  let inziali = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const nome = arr[i];
+    inziali.push(nome[0])
+  }
+  return inziali
+} */
+
+
+// arrow function
+
+const arrDiIniziali = arr =>{
+  let inziali = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const nome = arr[i];
+    inziali.push(nome[0])
+  }
+  return inziali
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+
+
+console.log(arrDiIniziali(names));
 
 
 
