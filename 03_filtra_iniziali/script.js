71d3a81fbcf2f912b109946ec977c23eca56f808
mattiacoli <mattia.coli@aol.com>
2025-02-17 16:04:45 +0100

    add filtra inizali function

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,38 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+/* let nomiCheInizianoPer = []
+
+for (let i = 0; i < names.length; i++) {
+  const nome = names[i];
+
+  if(nome.indexOf('A') === 0){
+    nomiCheInizianoPer.push(nome)
+  }
+  
+}
+console.log(nomiCheInizianoPer); */
 
 // Dichiara la funzione qui.
 
+function arrNomiConIniziale(letter, arr) {
+  let nomiCheInizianoPer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const nome = arr[i];
+
+    if (nome.indexOf(letter) === 0) {
+      nomiCheInizianoPer.push(nome)
+    }    
+  }
+  
+  return nomiCheInizianoPer
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const nomiConInizialeA = arrNomiConIniziale('A', names)
+console.log(nomiConInizialeA);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
